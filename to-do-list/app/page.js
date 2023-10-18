61b128d092fe8f75666d7d67976e377a6c9c8ad0
Tanner90909 <tmmcdaniel909@gmail.com>
2023-10-18 14:48:56 -0400

    init project

---
+++ to-do-list/app/page.js
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      
+    </main>
+  )
+}
